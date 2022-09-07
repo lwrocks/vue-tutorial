eebbd62aa9605774c155f82bfbe577dd651b1b04
Laurel Wright <wright.laureld@gmail.com>
2022-09-07 19:36:20 +0200

    HTML component section finished

--- app.js
+++ app.js
@@ -4,15 +4,38 @@ const app = Vue.createApp({
       url: "https://www.twitter.com",
       showBooks: true,
       books: [
-        { title: "Name of the Wind", author: "Patrick Rothfuss" },
-        { title: "The Way of Kings", author: "Brandon Sanderson" },
-        { title: "The Final Empire", author: "Brandon Sanderson" },
+        {
+          title: "Name of the Wind",
+          author: "Patrick Rothfuss",
+          img: "assets/1.jpeg",
+          isFav: true,
+        },
+        {
+          title: "The Way of Kings",
+          author: "Brandon Sanderson",
+          img: "assets/2.jpeg",
+          isFav: false,
+        },
+        {
+          title: "The Final Empire",
+          author: "Brandon Sanderson",
+          img: "assets/3.jpeg",
+          isFav: true,
+        },
       ],
     };
   },
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
+    },
+    toggleFav(book) {
+      book.isFav = !book.isFav;
+    },
+  },
+  computed: {
+    filteredBooks() {
+      return this.books.filter((book) => book.isFav);
     },
   },
 });
